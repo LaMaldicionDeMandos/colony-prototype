a2fab521bd41a9cb9f853294ac9673f49de11c55
Pasut Marcelo <pasutmarcelo@gmail.com>
2024-07-21 21:39:59 -0300

    trabajo en la animacion de las personas

---
+++ Colony-Prototype/Assets/Scripts/MainScene/Walk.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour {
+
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("left_walk", false);
+        animator.SetBool("up_walk", false);
+        animator.SetBool("right_walk", false);
+        animator.SetBool("down_walk", false);
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            animator.SetBool("left_walk", true);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            animator.SetBool("up_walk", true);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            animator.SetBool("down_walk", true);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            animator.SetBool("right_walk", true);
+        }
+    }
+}
