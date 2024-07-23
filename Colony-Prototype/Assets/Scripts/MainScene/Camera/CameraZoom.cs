462352e00f6d6d01d7941f62c2276805338661dd
Pasut Marcelo <pasutmarcelo@gmail.com>
2024-07-23 16:46:44 -0300

    implemento el zoom de la camara, tendria que mejararla un poco

--- Colony-Prototype/Assets/Scripts/MainScene/Camera/CameraZoom.cs
+++ Colony-Prototype/Assets/Scripts/MainScene/Camera/CameraZoom.cs
@@ -5,16 +5,16 @@ using UnityEngine.U2D;
 
 public class CameraZoom : MonoBehaviour {
 
-    private int ZOOM_X_DELTA = 16;
+    private static int ZOOM_X_DELTA = 16;
     private int ZOOM_Y_DELTA = 9;
 
-    private int ZOOM_FACTOR = 5;
+    private static int ZOOM_FACTOR = 5;
 
-    private int MIN_ZOOM_X = 240;
-    private int MIN_ZOOM_Y = 135;
+    private static int MIN_ZOOM_X = 240;
+    private static int MIN_ZOOM_Y = 135;
 
-    private int MIN_LEVEL = 0;
-    private int MAX_LEVEL = 6;
+    private static int MIN_LEVEL = 0;
+    private static int MAX_LEVEL = 6;
 
     private PixelPerfectCamera pixelPerfectCamera;
     public int zoomLevel = 1;
@@ -33,7 +33,9 @@ public class CameraZoom : MonoBehaviour {
     }
 
     void Zoom() {
-        pixelPerfectCamera.refResolutionX = Mathf.FloorToInt(MIN_ZOOM_X + zoomLevel*ZOOM_FACTOR*ZOOM_X_DELTA);
-		pixelPerfectCamera.refResolutionY = Mathf.FloorToInt(MIN_ZOOM_Y + zoomLevel*ZOOM_FACTOR*ZOOM_Y_DELTA);
+        int zoomX = Mathf.FloorToInt(MIN_ZOOM_X + zoomLevel*ZOOM_FACTOR*ZOOM_X_DELTA);
+        int zoomY = Mathf.FloorToInt(MIN_ZOOM_Y + zoomLevel*ZOOM_FACTOR*ZOOM_Y_DELTA);
+        pixelPerfectCamera.refResolutionX = zoomX;
+		pixelPerfectCamera.refResolutionY = zoomY;
     }
 }
