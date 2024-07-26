cca32da59df1daca1fa66e06c0398c4276c73e8d
Pasut Marcelo <pasutmarcelo@gmail.com>
2024-07-26 13:21:45 -0300

    trabajo en el texto y el calculo del tiempo actual

--- Colony-Prototype/Assets/Scripts/MainScene/World/TimeMachine.cs
+++ Colony-Prototype/Assets/Scripts/MainScene/World/TimeMachine.cs
@@ -7,10 +7,24 @@ public class TimeMachine : MonoBehaviour {
     private static float UNIT = 1.0f;
     private static int MINUTE_IN_SECONDS = 60;
     private static int HOUR_IN_MINUTES = 60;
+    private static int DAY_IN_HOURS = 24;
 
-    private float currentSecondsFraction;
-    private int currentSeconds;
-    private int currentMinutes;
+    private static int MONTH_IN_DAYS = 20;
+    private static int YEAR_IN_MONTHS = 4;
+
+    private static string[] FORMATTED_MONTH = new string[] { "Spring", "Summer", "Fall", "Winter"};
+
+    private float currentSecondFraction;
+    private int CurrentSecond;
+    public int CurrentMinute;
+
+    public int CurrentHour;
+
+    public int CurrentDay = 0;
+
+    public int CurrentMonth;
+
+    public int CurrentYear = 0;
 
     [SerializeField]
     public GameObject Label;
@@ -23,40 +37,47 @@ public class TimeMachine : MonoBehaviour {
     }
 
     void Update() {
-        int ds = UpdateSecondsFranction(Time.deltaTime);
-        int dm = UpdateSeconds(ds);
-        int dh = UpdateMinutes(dm);
-        Debug.Log("F: " + currentSecondsFraction + " -- s: " + currentSeconds + " -- m: " + currentMinutes + " -- h: " + dh);
+        printTime();
+    }
+
+    private int UpdateTime(ref int time, int delta, int module) {
+        time+= delta;
+        if (time >= module) {
+            time-= module;
+            return 1;
+        } else {
+            return 0;
+        }        
     }
 
     private int UpdateSecondsFranction(float dt) {
-        currentSecondsFraction+= dt;
-        if (currentSecondsFraction > UNIT) {
-            currentSecondsFraction-= UNIT;
+        currentSecondFraction+= dt;
+        if (currentSecondFraction > UNIT) {
+            currentSecondFraction-= UNIT;
             return 1;
         } else {
             return 0;
         }
     }
 
-    private int UpdateSeconds(int ds) {
-        currentSeconds+= ds;
-        if (currentSeconds >= MINUTE_IN_SECONDS) {
-            currentSeconds-= MINUTE_IN_SECONDS;
-            return 1;
-        } else {
-            return 0;
-        }
+    private void CalculateNewTime() {
+        int ds = UpdateSecondsFranction(Time.deltaTime);
+        int dm = UpdateTime(ref CurrentSecond, ds, MINUTE_IN_SECONDS);
+        int dh = UpdateTime(ref CurrentMinute, dm, HOUR_IN_MINUTES);
+        int dd = UpdateTime(ref CurrentHour, dh, DAY_IN_HOURS);
+        int dM = UpdateTime(ref CurrentDay, dd, MONTH_IN_DAYS);
+        int dy = UpdateTime(ref CurrentMonth, dM, YEAR_IN_MONTHS);
+        CurrentYear+= dy;
     }
 
-    private int UpdateMinutes(int dm) {
-        currentMinutes+= dm;
-        if (currentMinutes >= HOUR_IN_MINUTES) {
-            currentMinutes-= HOUR_IN_MINUTES;
-            return 1;
-        } else {
-            return 0;
-        }
+    private void printTime() {
+        CalculateNewTime();
+        string formattedTime = $"{CurrentHour:D2}:{CurrentMinute:D2} Day {(CurrentDay + 1)}, {FormatMonth(CurrentMonth)} Year {(CurrentYear + 1)}";
+        labelText.text = formattedTime;     
+    }
+
+    private string FormatMonth(int month) {
+        return FORMATTED_MONTH[month];
     }
     
 }
