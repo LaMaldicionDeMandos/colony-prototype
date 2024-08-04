0429925da753be57e01ffdf7333fb309bd4680e1
Pasut Marcelo <pasutmarcelo@gmail.com>
2024-08-04 11:44:28 -0300

    05:06 Trabajo en la pantalla de nuevo juego generero la semilla aleatoria

--- Colony-Prototype/Assets/Scripts/MainMenuScene/NewGameAction.cs
+++ Colony-Prototype/Assets/Scripts/MainMenuScene/NewGameAction.cs
@@ -6,13 +6,13 @@ using UnityEngine.SceneManagement;
 public class NewGameAction : MonoBehaviour
 {
 
-    private readonly static string MAIN_SCENE = "MainScene";
+    private readonly static string NEW_GAME_SCENE = "NewGameScene";
 
     void Start() {}
 
     void Update() {}
 
     public void OnPlay() {
-        SceneManager.LoadScene(MAIN_SCENE);
+        SceneManager.LoadScene(NEW_GAME_SCENE);
     }
 }
