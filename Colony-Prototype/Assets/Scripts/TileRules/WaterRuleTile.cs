e9acd88ab863f520aa8da93867948aa454e8612b
Pasut Marcelo <pasutmarcelo@gmail.com>
2024-08-09 10:21:26 -0300

    06:00 trabajo en definir las reglas de tiles de Agua poco profunda

--- Colony-Prototype/Assets/Scripts/TileRules/WaterRuleTile.cs
+++ Colony-Prototype/Assets/Scripts/TileRules/WaterRuleTile.cs
@@ -6,18 +6,16 @@ using UnityEngine.Tilemaps;
 [CreateAssetMenu(menuName = "2D/Custom Water Rule Tile")]
 public class WaterTileRule : RuleTile<WaterTileRule.Neighbor> {
     public class Neighbor : RuleTile.TilingRule.Neighbor {
-        public const int DeepWater = 2; // Define un estado adicional para agua profunda
-        public const int Shore = 3; // Define un estado adicional para costa
+        public const int DeepWater = 3; // Define un estado adicional para agua profunda
+        public const int Sand = 4; // Define un estado adicional para costa
     }
 
     public override bool RuleMatch(int neighbor, TileBase tile) {
         switch (neighbor)
         {
             case Neighbor.DeepWater:
-                // Comprueba si el tile vecino es agua profunda
                 return tile != null && tile.name == "DeepWaterDeepWater_Tile_Rule"; // Cambia el nombre según corresponda
-            case Neighbor.Shore:
-                // Comprueba si el tile vecino es costa
+            case Neighbor.Sand:
                 return tile != null && tile.name == "Sand_Tile_Rule"; // Cambia el nombre según corresponda
         }
         return base.RuleMatch(neighbor, tile);
