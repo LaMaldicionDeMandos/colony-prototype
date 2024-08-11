5f34f6fd64ee62e9d20661fe09a541041e7ecc61
Pasut Marcelo <pasutmarcelo@gmail.com>
2024-08-11 17:35:56 -0300

    01:20 implemento la lectura de multiples sprites

--- Colony-Prototype/Assets/Scripts/MainScene/World/Inventory/InventoryItemComponent.cs
+++ Colony-Prototype/Assets/Scripts/MainScene/World/Inventory/InventoryItemComponent.cs
@@ -1,10 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+interface SpriteLoaderStrategy {
+    Sprite LoadSprite(string spriteName);
+}
+
+class SingleSpriteLoaderStrategy : SpriteLoaderStrategy {
+    public Sprite LoadSprite(string spriteName) {
+        return Resources.Load<Sprite>(spriteName);
+    }
+}
+
+class MultipleSpriteLoaderStrategy : SpriteLoaderStrategy {
+    private char[] separators = new char[] { '.' };
+    public Sprite LoadSprite(string spriteName) {
+        string[] parts = spriteName.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+        Sprite[] sprites = Resources.LoadAll<Sprite>(parts[0]);
+        return System.Array.Find(sprites, sprite => sprite.name == parts[1]); 
+    }
+}
+
 public class InventoryItemComponent : MonoBehaviour {
+    private static SingleSpriteLoaderStrategy singleSpriteLoader = new SingleSpriteLoaderStrategy();
+    private static MultipleSpriteLoaderStrategy multipleSpriteLoader = new MultipleSpriteLoaderStrategy();
     public ItemInventory itemInventory;
 
     private TMP_Text label;
@@ -16,7 +38,7 @@ public class InventoryItemComponent : MonoBehaviour {
         icon = gameObject.transform.Find("Icon").gameObject;
         iconImage = icon.GetComponent<Image>();
         if (itemInventory.icon != null) {
-            Sprite sprite = Resources.Load<Sprite>(itemInventory.icon);
+            Sprite sprite = GetItemSprite(itemInventory.icon);
             iconImage.sprite = sprite;
         }
     }
@@ -28,5 +50,10 @@ public class InventoryItemComponent : MonoBehaviour {
         } else {
             icon.SetActive(false);
         }
+    }
+
+    private Sprite GetItemSprite(string spriteName) {
+        SpriteLoaderStrategy loader = (spriteName.Contains('.')) ? multipleSpriteLoader : singleSpriteLoader;
+        return loader.LoadSprite(spriteName);
     }
 }
