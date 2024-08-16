2764e1ac9b59051eb4d878a95d896cf5c09e4e33
Pasut Marcelo <pasutmarcelo@gmail.com>
2024-08-16 14:18:01 -0300

    00:07 trabajo en las especificaciones de persona

---
+++ Colony-Prototype/Assets/Scripts/MainScene/Person/PersonStats.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonStats : MonoBehaviour {
+    public PersonSpec spec;
+    void Start() {}
+
+    void Update() {}
+}
