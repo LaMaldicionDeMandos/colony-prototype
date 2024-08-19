92e872f93e2fcb290a12a69af7147e4756d1ffe6
Pasut Marcelo <pasutmarcelo@gmail.com>
2024-08-19 17:18:43 -0300

    06:53 El doble click del panel de personaje centra la camara en el personaje

--- Colony-Prototype/Assets/Scripts/MainScene/World/HUD/PersonPanelHandler.cs
+++ Colony-Prototype/Assets/Scripts/MainScene/World/HUD/PersonPanelHandler.cs
@@ -1,10 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using TMPro;
 
-public class PersonPanelHandler : MonoBehaviour {
-
+public class PersonPanelHandler : MonoBehaviour, IPointerClickHandler {
     public GameObject person;
     private PersonStats personStats;
 
@@ -19,5 +19,20 @@ public class PersonPanelHandler : MonoBehaviour {
 
     public void OnSelect() {
         Debug.Log("Click on person " + personStats.personName);
+    }
+
+    void OnDoubleClick() {
+        Debug.Log("Double click  on person " + personStats.personName);
+        Transform cameraTransform = Camera.main.transform;
+        cameraTransform.position = new Vector3(person.transform.position.x, person.transform.position.y, cameraTransform.position.z);
+    }
+
+    public void OnPointerClick(PointerEventData eventData) {
+        if (eventData.clickCount == 1) {
+            OnSelect();
+        }
+        if (eventData.clickCount == 2) {
+            OnDoubleClick();
+        }
     }
 }
