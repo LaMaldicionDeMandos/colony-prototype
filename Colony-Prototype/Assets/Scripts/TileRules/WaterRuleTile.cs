e182fcd19cff4d4b3168b52bb8cbe9a42841d2b0
Pasut Marcelo <pasutmarcelo@gmail.com>
2024-08-31 15:25:19 -0300

    03:55 Agrogo mas modelos de mescla de playa y tierras humedas

--- Colony-Prototype/Assets/Scripts/TileRules/WaterRuleTile.cs
+++ Colony-Prototype/Assets/Scripts/TileRules/WaterRuleTile.cs
@@ -6,15 +6,15 @@ using UnityEngine.Tilemaps;
 [CreateAssetMenu(menuName = "2D/Custom Water Rule Tile")]
 public class WaterTileRule : RuleTile<WaterTileRule.Neighbor> {
     public class Neighbor : RuleTile.TilingRule.Neighbor {
-        public const int DeepWater = 3; // Define un estado adicional para agua profunda
+        public const int Wet = 3; // Define un estado adicional para agua profunda
         public const int Sand = 4; // Define un estado adicional para costa
     }
 
     public override bool RuleMatch(int neighbor, TileBase tile) {
         switch (neighbor)
         {
-            case Neighbor.DeepWater:
-                return tile != null && tile.name == "DeepWaterDeepWater_Tile_Rule"; // Cambia el nombre según corresponda
+            case Neighbor.Wet:
+                return tile != null && tile.name == "WetZoneTileRule"; // Cambia el nombre según corresponda
             case Neighbor.Sand:
                 return tile != null && tile.name == "Sand_Tile_Rule"; // Cambia el nombre según corresponda
         }
